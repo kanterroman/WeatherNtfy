0a1491e791d17e3594460717893dba292c392002
Kanterov Roman <krd23u554@students.bmstu.ru>
2025-07-15 22:39:36 +0300

    test version of bot

---
+++ src/test/java/com/kanterroman/weatherntfy/WeatherNtfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanterroman.weatherntfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherNtfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
